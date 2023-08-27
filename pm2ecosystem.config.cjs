aeb03800279e596c43ef9f3abdc5e9458a4e6e08
Max Matthews <max@mzmtech.com>
2023-08-27 11:45:32 -0400

    Adjust cronjob to only run every 3 hours

--- pm2ecosystem.config.cjs
+++ pm2ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
 
 			instances: 1,
 			exec_mode: "fork",
-			cron_restart: "* 0/3 * * *",
+			cron_restart: "25 3,6,9,12,15,18,22 * * *",
 			watch: false,
 			autorestart: false,
 			node_args: "--no-warnings=ExperimentalWarning"
